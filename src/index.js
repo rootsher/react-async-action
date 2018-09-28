7b9bcf78b60e5a75d983ae028a57aafd20d9691e
Konrad Kowalski <rootsher@gmail.com>
2018-09-28 15:40:41 +0200

    Refactor Async - create Async factory function

--- src/index.js
+++ src/index.js
@@ -1,63 +1,74 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Async extends Component {
-    static propTypes = {
-        children: PropTypes.func.isRequired,
-        action: PropTypes.func.isRequired,
-        onDemand: PropTypes.bool,
-    };
+export const createInstance = (defaultProps = {}) => {
+    class Async extends Component {
+        static propTypes = {
+            children: PropTypes.func.isRequired,
+            action: PropTypes.func.isRequired,
+            onDemand: PropTypes.bool,
+        };
 
-    state = {
-        isLoading: false,
-        response: null,
-        error: null,
-    };
+        static defaultProps = {
+            onDemand: false,
+            ...defaultProps,
+        };
 
-    componentDidMount() {
-        if (!this.props.onDemand) {
-            this._handleAction();
+        state = {
+            isLoading: false,
+            response: null,
+            error: null,
+        };
+
+        componentDidMount() {
+            if (!this.props.onDemand) {
+                this._handleAction();
+            }
+        }
+
+        render() {
+            return this.props.children({
+                isPending: this._isPending(),
+                isLoading: this.state.isLoading,
+                response: this.state.response,
+                error: this.state.error,
+                run: () => this.props.onDemand && this._handleAction(),
+                reload: () => this._handleAction(),
+            });
+        }
+
+        _handleAction() {
+            const action = Promise.resolve(this.props.action());
+
+            this.setState({
+                isLoading: true,
+            });
+
+            action
+                .then(response => this.setState({
+                    response,
+                    isLoading: false,
+                }))
+                .catch(error => this.setState({
+                    error,
+                    isLoading: false,
+                }));
+        }
+
+        /**
+         * Returns true if the request has not yet been fired.
+         *
+         * @returns {boolean}
+         * @private
+         */
+        _isPending() {
+            const { isLoading, response, error } = this.state;
+
+            return (!isLoading && !response && !error);
         }
     }
 
-    render() {
-        return this.props.children({
-            isPending: this._isPending(),
-            isLoading: this.state.isLoading,
-            response: this.state.response,
-            error: this.state.error,
-            run: () => this.props.onDemand && this._handleAction(),
-            reload: () => this._handleAction(),
-        });
-    }
+    return Async;
+};
 
-    _handleAction() {
-        const action = Promise.resolve(this.props.action());
-
-        this.setState({
-            isLoading: true,
-        });
-
-        action
-            .then(response => this.setState({
-                response,
-                isLoading: false,
-            }))
-            .catch(error => this.setState({
-                error,
-                isLoading: false,
-            }));
-    }
-
-    /**
-     * Returns true if the request has not yet been fired.
-     *
-     * @returns {boolean}
-     * @private
-     */
-    _isPending() {
-        const { isLoading, response, error } = this.state;
-
-        return (!isLoading && !response && !error);
-    }
-}
+export default createInstance();
