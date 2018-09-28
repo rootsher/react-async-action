25de3c0faecb4916cede81449b9b4c9aa6f51f63
Konrad Kowalski <rootsher@gmail.com>
2018-09-28 16:26:17 +0200

    Create initial example - dependent requests

--- example/dependent-requests.example.js
+++ example/dependent-requests.example.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Async from 'react-async-action';
 
+const fetchProductToken = () => fetch('api/product/token');
+const fetchProductDetails = ({ token }) => fetch('api/product/1/details', { token });
+
+/* NOT WORKING YET */
 export default () => (
-    <Async action={() => fetch('api/product/list')}>
-        {({ isLoading, response, error }) => (
-            <React.Fragment>
-                {isLoading && <div>Loading...</div>}
-                {response && <pre>{JSON.stringify(response, null, '\t')}</pre>}
-                {error && <div style={{ color: 'red' }}>{JSON.stringify(error, null, '\t')}</div>}
-            </React.Fragment>
+    <Async action={fetchProductToken}>
+        {({ response: token }) => (
+            <Async action={fetchProductDetails} token={token}>
+                {({ response }) => (
+                    response && <pre>{JSON.stringify(response, null, '\t')}</pre>
+                )}
+            </Async>
         )}
     </Async>
 );
