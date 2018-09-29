4d5dc6b809fbc89d84488c19cb7d9f1783200145
Konrad Kowalski <rootsher@gmail.com>
2018-09-29 10:05:41 +0200

    Create example of use dependent requests

--- example/dependent-requests.example.js
+++ example/dependent-requests.example.js
@@ -4,15 +4,18 @@ import Async from 'react-async-action';
 const fetchProductToken = () => fetch('api/product/token');
 const fetchProductDetails = ({ token }) => fetch('api/product/1/details', { token });
 
-/* NOT WORKING YET */
 export default () => (
     <Async action={fetchProductToken}>
-        {({ response: token }) => (
-            <Async action={fetchProductDetails} token={token}>
-                {({ response }) => (
-                    response && <pre>{JSON.stringify(response, null, '\t')}</pre>
-                )}
-            </Async>
-        )}
+        <Async.Resolved>
+            {token => (
+                <Async action={fetchProductDetails} token={token}>
+                    <Async.Resolved>
+                        {response => (
+                            <pre>{JSON.stringify(response, null, '\t')}</pre>
+                        )}
+                    </Async.Resolved>
+                </Async>
+            )}
+        </Async.Resolved>
     </Async>
 );
