c9d7a2e73e6757b030fa4efd53190cd85950fdb9
Konrad Kowalski <rootsher@gmail.com>
2018-09-30 19:25:49 +0200

    Repair async-x example

--- example/simple/data-request-async-x.example.js
+++ example/simple/data-request-async-x.example.js
@@ -3,12 +3,14 @@ import Async from 'react-async-action';
 
 export default () => (
     <Async action={() => fetch('api/product/list')}>
-        {({ isLoading, response, error }) => (
-            <React.Fragment>
-                {isLoading && <div>Loading...</div>}
-                {response && <pre>{JSON.stringify(response, null, '\t')}</pre>}
-                {error && <div style={{ color: 'red' }}>{JSON.stringify(error, null, '\t')}</div>}
-            </React.Fragment>
-        )}
+        <Async.Loading>
+            <div>Loading...</div>
+        </Async.Loading>
+        <Async.Resolved>
+            {response => <pre>{JSON.stringify(response, null, '\t')}</pre>}
+        </Async.Resolved>
+        <Async.Rejected>
+            {error => <pre>{JSON.stringify(error, null, '\t')}</pre>}
+        </Async.Rejected>
     </Async>
 );
