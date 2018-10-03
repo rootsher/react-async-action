485038aa1a69c52cf908ffebf21a548425f0b65b
Konrad Kowalski <rootsher@gmail.com>
2018-10-03 11:03:36 +0200

    Create example for response-transformer

---
+++ example/response-transformer.example.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Async from 'react-async-action';
+
+export default () => (
+    <Async
+		action={() => fetch('api/product/list')}
+		transformer={response => ({
+			...response,
+			someKey: 'someValue'
+		})}
+	>
+		<Async.Resolved>
+			{response => <pre>{response.someKey}</pre>}
+		</Async.Resolved>
+    </Async>
+);
