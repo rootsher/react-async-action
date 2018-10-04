1d714b2dd4ab32036ec9c6d960d4754ede9417a4
Konrad Kowalski <rootsher@gmail.com>
2018-10-04 10:59:41 +0200

    Create example of cancel request

---
+++ example/simple/cancel-request.example.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import Async from 'react-async-action';
+
+export default () => (
+    <Async action={() => fetch('api/product/list')} delay={3000}>
+        {({ cancel, reload }) => (
+            <Fragment>
+                <button onClick={reload}>reload</button>
+                <button onClick={cancel}>cancel</button>
+                <Async.Loading>
+                    <div>Loading...</div>
+                </Async.Loading>
+                <Async.Resolved>
+                    {response => <pre>{JSON.stringify(response, null, '\t')}</pre>}
+                </Async.Resolved>
+            </Fragment>
+        )}
+    </Async>
+);
